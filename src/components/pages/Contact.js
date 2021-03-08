3922113815fb91fd7a08b49cf630aecfc6a643a0
Joel Zisholtz <joelzisholtz@Joels-MBP.attlocal.net>
2021-03-08 01:51:19 -0600

    contact form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,38 +1,110 @@
-import React from 'react';
-import '../../App.css';
+import React, { useState } from 'react';
+import '../Contact.css';
 import emailjs from 'emailjs-com';
+import { useForm } from 'react-hook-form'
+import { init, sendForm } from 'emailjs-com';
+init('user_J2qvlJay43uXxnGDwFxU2')
 
 
-function Contact() {
+const Contact = () => {
+const { register, handleSubmit, watch, errors } = useForm();
+const [statusMessage, setStatusMessage] = useState("Message");
+const [contactNumber, setContactNumber] = useState("000000");
 
-    function sendEmail(e) {
-      e.preventDefault();
+
+
+const generateContactNumber = () => {
+  const numStr = "000000" + (Math.random() * 1000000 | 0);
+  setContactNumber(numStr.substring(numStr.length - 6))
+}
+
+const onSubmit = (data) => {
+  const form = document.querySelector('#contact-form')
+  const statusMsg = document.querySelector('.status-message')
+}
+
+generateContactNumber();
+
+// sendForm('service_iyjivmb', 'template_rdw5vx9', '#contact-form')
+//         .then((result) => {
+//             console.log(result.text);
+//         }, (error) => {
+//             console.log(error.text);
+//         });
+//         e.target.reset()
+//     }
+// function sendEmail(e) {
+//       e.preventDefault();
+
+}
+
   
-      emailjs.sendForm('service_iyjivmb', 'template_rdw5vx9', e.target, 'user_J2qvlJay43uXxnGDwFxU2')
-        .then((result) => {
-            console.log(result.text);
-        }, (error) => {
-            console.log(error.text);
-        });
-        e.target.reset()
-    }
+//       emailjs.sendForm('service_iyjivmb', 'template_rdw5vx9', e.target, 'user_J2qvlJay43uXxnGDwFxU2')
+//         .then((result) => {
+//             console.log(result.text);
+//         }, (error) => {
+//             console.log(error.text);
+//         });
+//         e.target.reset()
+//     }
+
+const message = watch('message') || "";
+const messageCharsLeft = 1500 - message.length;
   
     return (
-      <form className="contact-form" onSubmit={sendEmail}>
-        <input type="hidden" name="contact_number" />
-        <label>Name</label>
-        <input type="text" name="user_name" />
-        <label>Email</label>
-        <input type="email" name="user_email" />
-        <label>Phone</label>
-        <input type="phone" name="user_phone" />
-        <label>Company</label>
-        <input type="company" name="user_company" />
-        <label>Message</label>
-        <textarea name="message" />
+      <>
+      <h1>Contact</h1>
+       <form id="contact-form" onSubmit={handleSubmit(onSubmit)}>
+        <input type="hidden" name="contact_number" value={contactNumber} />
+       
+        {/* <label>Name</label> */}
+        {errors.user_name && errors.user_name.type === "required" && (<div role="alert">*Name is required<br/></div>)}
+        <input 
+        type="text" 
+        name="user_name" 
+        placeholder='Name'
+        maxLength='30'
+        aria-invalid={errors.user_name ? "true" : "false"}
+        ref={register({ required: true})}
+        /> 
+        {/* <label>Email</label> */}
+        {errors.user_email && errors.user_email.type === "required" && (<div role="alert">*Email is required<br/></div>)}
+        <input 
+        type="email" 
+        name="user_email" 
+        placeholder='E-mail'
+        maxLength='30'
+        aria-invalid={errors.user_name ? "true" : "false"}
+        ref={register({ required: true})}
+        />
+        {/* <label>Phone</label> */}
+        <input 
+        type="phone" 
+        name="user_phone" 
+        placeholder='Phone'
+        />
+        {/* <label>Company</label> */}
+        <input 
+        type="company" 
+        name="user_company" 
+        placeholder='Company' 
+        />
+        {/* <label>Message</label> */}
+        {errors.message && errors.message.type === "required" && (<div role="alert">*A message is required<br/></div>)}
+        <textarea 
+        name="message" 
+        placeholder='Message'
+        maxLength='1500'
+        aria-invalid={errors.user_name ? "true" : "false"}
+        ref={register({ required: true})}
+        />
+        <p className='message-chars-left'>{messageCharsLeft}</p>
+        <br/>
         <input type="submit" value="Send" />
       </form>
+     </>
     );
   }
 
   export default Contact
+
